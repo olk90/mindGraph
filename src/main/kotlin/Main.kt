ad19e821b4c14d185b70fc60b56ae5c4989c84f0
Oliver Köhler <olk.git@posteo.eu>
2024-10-10 22:27:14 +0200

    minor refactorings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,7 @@
 package de.olk90
 
+import de.olk90.logic.renderMermaidMindMap
 import org.graphstream.graph.Graph
-import org.graphstream.graph.Node
-import org.graphstream.graph.implementations.SingleGraph
 import org.graphstream.ui.view.Viewer.CloseFramePolicy
 
 fun main() {
@@ -13,100 +12,9 @@ fun main() {
 
     // Create a viewer to display the graph
     val viewer = graph.display()
-    viewer.setCloseFramePolicy(CloseFramePolicy.EXIT)
+    viewer.closeFramePolicy = CloseFramePolicy.EXIT
 
     // Show the viewer window
     viewer.enableAutoLayout()
 }
 
-fun renderMermaidMindMap(): Graph {
-    val content = """mindmap
-Nutzen und Ziele
-	Reduktion von Komplexität
-	Erreichtung der Qualitätsanforderungen
-		Zuverlässigkeit
-		Wartbarkeit
-		Änderbarkeit
-		Sicherheit
-		Energieeffizienz
-	Sicherstellung der Erfüllbarkeit funktionaler Anforderungen
-	Unterstützung vom Entwurf bis zur Weiterentwicklung
-	Vermittlung von Verständnis
-	Spezifikation architekturrelevanter Richtlinien
-"""
-
-    // Parse the Mermaid mind map content
-    val lines = content.lines().filter { it.isNotEmpty() }
-    if (lines.isEmpty() || !lines[0].trim().startsWith("mindmap")) {
-        throw IllegalArgumentException("Invalid Mermaid mind map format.")
-    }
-
-    // Create a new GraphStream graph
-    val graph: Graph = SingleGraph("MindMap")
-
-    val rootId = lines[1].trim()
-    val root = graph.addNode(rootId)
-    root.setAttribute("ui.label", rootId)
-
-    val nodeMap = mutableMapOf<Node, MutableList<Node>>()
-
-    lines.drop(2).forEachIndexed { idx, str ->
-
-        val offsetIdx = idx + 2
-        val nodeId = str.trim()
-        val node = graph.addNode(nodeId)
-        node.setAttribute("ui.label", nodeId)
-        val predecessor = lines[offsetIdx - 1]
-        val predecessorLevel = getLevel(predecessor)
-        val predecessorNode = graph.getNode(predecessor.trim())
-        val level = getLevel(str)
-
-        when {
-            level > predecessorLevel -> {
-                appendNode(nodeMap, predecessorNode, node)
-                graph.addEdge("${predecessorNode.id}-$nodeId", predecessorNode, node)
-            }
-
-            level == predecessorLevel -> {
-                val parentNode = findKeyContainingNode(nodeMap, predecessorNode)
-                appendNode(nodeMap, parentNode, node)
-                graph.addEdge("${parentNode.id}-${node.id}", parentNode, node)
-            }
-
-            else -> {
-                val siblingNode = findKeyContainingNode(nodeMap, predecessorNode)
-                val parentNode = findKeyContainingNode(nodeMap, siblingNode)
-                appendNode(nodeMap, parentNode, node)
-                graph.addEdge("${parentNode.id}-${node.id}", parentNode, node)
-            }
-        }
-    }
-
-    // Set attributes for visualization
-    val css =
-        ::renderMermaidMindMap.javaClass.classLoader.getResource("style.css")!!.toExternalForm()
-    graph.setAttribute("ui.stylesheet", css)
-    return graph
-}
-
-private fun getLevel(string: String): Int {
-    return string.takeWhile { it == '\t' }.length
-}
-
-fun appendNode(nodeMap: MutableMap<Node, MutableList<Node>>, key: Node, newNode: Node) {
-    if (nodeMap.containsKey(key)) {
-        nodeMap[key]!!.add(newNode)
-    } else {
-        nodeMap[key] = mutableListOf(newNode)
-    }
-}
-
-fun findKeyContainingNode(nodeMap: MutableMap<Node, MutableList<Node>>, targetNode: Node): Node {
-    for ((key, nodeList) in nodeMap) {
-        if (nodeList.contains(targetNode)) {
-            return key
-        }
-    }
-//    throw IllegalArgumentException("Node ${targetNode.id} not found in any list")
-    return targetNode
-}
---
+++ src/main/kotlin/logic/Rendering.kt
@@ -0,0 +1,99 @@
+package de.olk90.logic
+
+import org.graphstream.graph.Graph
+import org.graphstream.graph.Node
+import org.graphstream.graph.implementations.SingleGraph
+
+fun renderMermaidMindMap(): Graph {
+    val content = """mindmap
+Nutzen und Ziele
+	Reduktion von Komplexität
+	Erreichtung der Qualitätsanforderungen
+		Zuverlässigkeit
+		Wartbarkeit
+		Änderbarkeit
+		Sicherheit
+		Energieeffizienz
+	Sicherstellung der Erfüllbarkeit funktionaler Anforderungen
+	Unterstützung vom Entwurf bis zur Weiterentwicklung
+	Vermittlung von Verständnis
+	Spezifikation architekturrelevanter Richtlinien
+"""
+
+    // Parse the Mermaid mind map content
+    val lines = content.lines().filter { it.isNotEmpty() }
+    if (lines.isEmpty() || !lines[0].trim().startsWith("mindmap")) {
+        throw IllegalArgumentException("Invalid Mermaid mind map format.")
+    }
+
+    // Create a new GraphStream graph
+    val graph: Graph = SingleGraph("MindMap")
+
+    val rootId = lines[1].trim()
+    val root = graph.addNode(rootId)
+    root.setAttribute("ui.label", rootId)
+
+    val nodeMap = mutableMapOf<Node, MutableList<Node>>()
+
+    lines.drop(2).forEachIndexed { idx, str ->
+
+        val offsetIdx = idx + 2
+        val nodeId = str.trim()
+        val node = graph.addNode(nodeId)
+        node.setAttribute("ui.label", nodeId)
+        val predecessor = lines[offsetIdx - 1]
+        val predecessorLevel = getLevel(predecessor)
+        val predecessorNode = graph.getNode(predecessor.trim())
+        val level = getLevel(str)
+
+        when {
+            level > predecessorLevel -> {
+                appendNode(nodeMap, predecessorNode, node)
+                graph.addEdge("${predecessorNode.id}-$nodeId", predecessorNode, node)
+            }
+
+            level == predecessorLevel -> {
+                val parentNode = findKeyContainingNode(nodeMap, predecessorNode)
+                appendNode(nodeMap, parentNode, node)
+                graph.addEdge("${parentNode.id}-${node.id}", parentNode, node)
+            }
+
+            else -> {
+                val siblingNode = findKeyContainingNode(nodeMap, predecessorNode)
+                val parentNode = findKeyContainingNode(nodeMap, siblingNode)
+                appendNode(nodeMap, parentNode, node)
+                graph.addEdge("${parentNode.id}-${node.id}", parentNode, node)
+            }
+        }
+    }
+
+    // Set attributes for visualization
+    val css =
+        ::renderMermaidMindMap.javaClass.classLoader
+            .getResourceAsStream("graph.css")!!
+            .bufferedReader()
+            .useLines { it.joinToString("\n") }
+    graph.setAttribute("ui.stylesheet", css)
+    return graph
+}
+
+private fun getLevel(string: String): Int {
+    return string.takeWhile { it == '\t' }.length
+}
+
+fun appendNode(nodeMap: MutableMap<Node, MutableList<Node>>, key: Node, newNode: Node) {
+    if (nodeMap.containsKey(key)) {
+        nodeMap[key]!!.add(newNode)
+    } else {
+        nodeMap[key] = mutableListOf(newNode)
+    }
+}
+
+fun findKeyContainingNode(nodeMap: MutableMap<Node, MutableList<Node>>, targetNode: Node): Node {
+    for ((key, nodeList) in nodeMap) {
+        if (nodeList.contains(targetNode)) {
+            return key
+        }
+    }
+    return targetNode
+}
