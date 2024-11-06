e65eca4728de5f370134c524edd78bb329859929
Oliver Köhler <olk.git@posteo.eu>
2024-11-06 10:43:28 +0100

    Added line numbering
    (cherry picked from commit f9b73bf2332e5a572ec69c0b331502e8247bef54)

--- src/main/kotlin/de/olk90/codeTextField/view/CodeTextField.kt
+++ src/main/kotlin/de/olk90/codeTextField/view/CodeTextField.kt
@@ -1,12 +1,18 @@
 package de.olk90.codeTextField.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import de.olk90.codeTextField.logic.formatText
 
 @Composable
@@ -18,11 +24,29 @@ fun CodeTextField(
         formatText(content, it)
     }
 
-    TextField(
-        value = content.value,
-        onValueChange = updateContent(),
-        modifier = modifier,
-        textStyle = MaterialTheme.typography.body1.copy(fontFamily = FontFamily.Monospace),
-        maxLines = Int.MAX_VALUE
-    )
+    var lineTops by remember { mutableStateOf(emptyArray<Float>()) }
+
+    Row {
+        if (lineTops.isNotEmpty()) {
+            Box(modifier = Modifier.padding(horizontal = 4.dp)) {
+                lineTops.forEachIndexed { index, top ->
+                    Text(
+                        modifier = Modifier.offset(
+                            y = with(LocalDensity.current) { top.toDp() }), text = (index + 1).toString()
+                    )
+                }
+            }
+        }
+        BasicTextField(
+            value = content.value,
+            onValueChange = updateContent(),
+            onTextLayout = { result ->
+                lineTops = Array(result.lineCount) { result.getLineTop(it) }
+            },
+            modifier = modifier,
+            textStyle = MaterialTheme.typography.body1.copy(fontFamily = FontFamily.Monospace),
+            maxLines = Int.MAX_VALUE
+        )
+    }
+
 }
